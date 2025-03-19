c789dd393165c355d7aca237ba069616270fa3dd
nexuster <coralpinklmao@outlook.com>
2025-03-19 09:05:12 -0400

    i hate js

---
+++ script.js
@@ -0,0 +1,20 @@
+let x, y, radius = 5;
+function coordinate(event) {
+    x = event.clientX;
+    y = event.clientY;
+}
+
+window.onload = function() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.addEventListener("mousedown", function(event) {
+        coordinate(event);
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    });
+
+    canvas.addEventListener("mouseup", function(event) {
+        ctx.stroke();
+    });
+}
